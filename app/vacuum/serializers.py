5774bd1099096902339d80321ad392db8eae5f18
Mostafizur Rahman <csemrm@gmail.com>
2023-09-03 19:15:58 -0700

    Docstrings added

--- app/vacuum/serializers.py
+++ app/vacuum/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 
 class VacuumCleanerSerializer(serializers.Serializer):
+    """Serializer for Robot Vacuum Cleaner Challenge.
+
+    Serializer will accept two lists. 1.  cleaning_batches:  cleaning
+    instructions as an array of arrays (cleaning batches).
+    2. another is priority room is in the cleaning batch commands
+    called ‘Priority Rooms’ It receives the command as a single array
+    """
     cleaning_batches = serializers.ListField(child=serializers.ListField())
     priority_rooms = serializers.ListField()
--- app/vacuum/views.py
+++ app/vacuum/views.py
@@ -1,11 +1,39 @@
+"""
+Views for the Vacuum Cleaner APIs.
+"""
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import VacuumCleanerSerializer
 
+
 class VacuumCleanerDataView(APIView):
+    """
+        Vacuum Cleaner Data View will receive cleaning_batches, priority_rooms
+        data from the api/vacuum endpoint via the post method.
+    """
 
     def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests to clean rooms using a vacuum cleaner.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+            In request object cleaning_batches, priority_rooms data
+            will available for process implementation.
+
+        Returns:
+            Response: An HTTP response with cleaning results with 5
+            outputs Like:
+            traversal_path: The actual path the vacuum took as arrays of
+            rooms (An array of arrays showcasing its actual traversal of rooms)
+            total_cleaned: # of rooms it cleaned in total
+            total_batches: # of batches it processed
+            total_unvisited: # of rooms it passed without cleaning either
+            going left or right
+            final_room: The final (current) room the vacuum cleaner is in
+        """
         serializer = VacuumCleanerSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -15,8 +43,26 @@ class VacuumCleanerDataView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
     def vacuum_cleaner(self, cleaning_batches, priority_rooms):
+        """
+                Simulate a vacuum cleaner cleaning algorithm.
+
+                Args:
+            request (HttpRequest): The HTTP request object.
+            In request object cleaning_batches, priority_rooms data
+            will available for process implementation.
+
+        Returns:
+            An response with cleaning results with 5
+            outputs like:
+            traversal_path: The actual path the vacuum took as arrays of
+            rooms (An array of arrays showcasing its actual traversal of rooms)
+            total_cleaned: # of rooms it cleaned in total
+            total_batches: # of batches it processed
+            total_unvisited: # of rooms it passed without cleaning either
+            going left or right
+            final_room: The final (current) room the vacuum cleaner is in
+        """
         current_room = 1
         cleaned_rooms = set()
         traversed_rooms = []
@@ -31,48 +77,42 @@ class VacuumCleanerDataView(APIView):
             print('priority_room', priority_rooms_in_batch)
             # Visit priority rooms first
             for room in priority_rooms_in_batch:
-                print('priority_room: ',room, current_room)
+                print('priority_room: ', room, current_room)
                 # room = 7 < current_room =1
+                # checking cleaner moving directions for priority rooms
                 if room < current_room:
-                    rooms = range(current_room - 1, room - 1, -1)
-                    print('priority total_unvisited: ', total_unvisited)
-                    total_unvisited += len(rooms) - 1
-                    print('priority total_unvisited: ', total_unvisited)
+                    rooms = range(current_room - 1, room, -1)
+                    total_unvisited += len(rooms)
                     traversed_rooms.extend(rooms)
                 else:
                     rooms = range(current_room + 1, room)
                     total_unvisited += len(rooms)
-                    print('priority total_unvisited: ', total_unvisited)
-                    print('priority total_unvisited rooms: ', list(rooms))
                     traversed_rooms.extend(rooms)
                 current_room = room
-                print('current_room: ', current_room)
                 cleaned_rooms.add(current_room)
-                print('cleaned_rooms: ', cleaned_rooms)
                 total_cleaned += 1
-                print('total_cleaned: ', total_cleaned)
                 traversed_rooms.append(current_room)
-                print('traversed_rooms: ', traversed_rooms)
 
-            # Visit Non priority rooms in the batch
+            # Visit Non-priority rooms in the batch
             print('Visit rooms in the batch: ', batch)
             for room in batch:
                 print('Visit other room in the batch: ', room)
                 if room != current_room and room not in priority_rooms:
-                    print('Visit  room and current_room in the batch: ', room, current_room)
+                    print('Visit room and current_room in the batch: ', room, current_room)
+
+                    # checking cleaner moving directions for non-priority rooms
+
                     if room < current_room:
                         rooms = range(current_room - 1, room, -1)
                         total_unvisited += len(rooms)
                         traversed_rooms.extend(rooms)
                     else:
                         rooms = range(current_room + 1, room)
-                        print('Visit rooms: ', list(rooms))
                         total_unvisited += len(rooms)
                         traversed_rooms.extend(rooms)
 
                     current_room = room
                     cleaned_rooms.add(current_room)
-                    print('cleaned_rooms: ', cleaned_rooms)
                     total_cleaned += 1
                     traversed_rooms.append(current_room)
 
@@ -87,5 +127,3 @@ class VacuumCleanerDataView(APIView):
             "total_unvisited": total_unvisited,
             "final_room": current_room,
         }
-
-
